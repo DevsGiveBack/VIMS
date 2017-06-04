f5ded9e103c2e7c3a19abbc9e59280aa6814cb01
Brian Preston <brian8181@gmail.com>
2017-06-04 07:49:58 -0500

    simplify HomeController.cs

--- TJS.VIMS/TJS.VIMS/Controllers/HomeController.cs
+++ TJS.VIMS/TJS.VIMS/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace TJS.VIMS.Controllers
 {
     public class HomeController : Controller
     {
-        private ILookUpRepository lookUpRepository;
+        //private ILookUpRepository lookUpRepository;
 
         public HomeController()
         {
@@ -19,13 +19,17 @@ namespace TJS.VIMS.Controllers
 
         public HomeController(ILookUpRepository lookUpRepository)
         {
-            this.lookUpRepository = lookUpRepository;
+            //this.lookUpRepository = lookUpRepository;
         }
 
         public ActionResult Location()
         {
-            List<Location> lsLocation = lookUpRepository.GetLocations();
-            return View(new LocationViewModel(lsLocation));
+            //List<Location> lsLocation = lookUpRepository.GetLocations();
+            using (VIMSDBContext context = new VIMSDBContext())
+            {
+                List<Location> locations = context.Locations.ToList<Location>();
+                return View(new LocationViewModel(locations));
+            }
         }
 
         [HttpPost]      
@@ -43,9 +47,17 @@ namespace TJS.VIMS.Controllers
         public ActionResult AddLocation()
         {
             EditLocationsViewModel vm = new EditLocationsViewModel();
-            vm.countries = lookUpRepository.GetCountries();
-            vm.states = lookUpRepository.GetStates();
-            vm.Location = new Location();
+
+            //vm.countries = lookUpRepository.GetCountries();
+            //vm.states = lookUpRepository.GetStates();
+
+            using (VIMSDBContext context = new VIMSDBContext())
+            {
+                vm.countries = context.Countries.ToList<Country>();
+                vm.states = context.States.ToList<State>();
+                vm.Location = new Location();
+            }
+
             return View("AddLocation", vm);
         }
 
@@ -54,10 +66,16 @@ namespace TJS.VIMS.Controllers
         {
             if (ModelState.IsValid) 
             {
-                LocationRepository repo = new LocationRepository(new VIMSDBContext());
-                repo.Add(model.Location);
-                repo.Save();
-                repo.Dispose();
+                //LocationRepository repo = new LocationRepository(new VIMSDBContext());
+                //repo.Add(model.Location);
+                //repo.Save();
+                //repo.Dispose();
+
+                using (VIMSDBContext context = new VIMSDBContext())
+                {
+                    context.Locations.Add(model.Location);
+                    context.SaveChanges();
+                }
 
                 return RedirectToAction("Location");
             }
@@ -68,9 +86,18 @@ namespace TJS.VIMS.Controllers
         public ActionResult EditLocation(LocationViewModel model)
         {
             EditLocationsViewModel vm = new EditLocationsViewModel();
-            vm.countries = lookUpRepository.GetCountries();
-            vm.states = lookUpRepository.GetStates();
-            vm.Location = lookUpRepository.GetLocationById(model.SelectedLocationId);
+
+            //vm.countries = lookUpRepository.GetCountries();
+            //vm.states = lookUpRepository.GetStates();
+            //vm.Location = lookUpRepository.GetLocationById(model.SelectedLocationId);
+
+            using (VIMSDBContext context = new VIMSDBContext())
+            {
+                vm.countries = context.Countries.ToList<Country>();
+                vm.states = context.States.ToList<State>();
+                vm.Location = context.Locations.Find(model.SelectedLocationId);
+            }
+
             return View("EditLocation", vm);
         }
 
@@ -79,10 +106,17 @@ namespace TJS.VIMS.Controllers
         {
             if (ModelState.IsValid)
             {
-                LocationRepository repo = new LocationRepository(new VIMSDBContext());
-                Location location = repo.Find(model.Location.LocationId);
-                repo.Context.Entry(location).CurrentValues.SetValues(model.Location);
-                repo.Save();
+                //LocationRepository repo = new LocationRepository(new VIMSDBContext());
+                //Location location = repo.Find(model.Location.LocationId);
+                //repo.Context.Entry(location).CurrentValues.SetValues(model.Location);
+                //repo.Save();
+
+                using (VIMSDBContext context = new VIMSDBContext())
+                {
+                    Location location = context.Locations.Find(model.Location.LocationId);
+                    context.Entry(location).CurrentValues.SetValues(model.Location);
+                    context.SaveChanges();
+                }
 
                 return RedirectToAction("Location");
             }
@@ -96,11 +130,13 @@ namespace TJS.VIMS.Controllers
             //Location location = repo.Find(model.SelectedLocationId);
             //repo.Remove(location);
             //repo.Save();
-            
-            VIMSDBContext context = new VIMSDBContext();
-            Location location = context.Locations.Find(model.SelectedLocationId);
-            context.Locations.Remove(location);
-            context.SaveChanges();
+
+            using (VIMSDBContext context = new VIMSDBContext())
+            {
+                Location location = context.Locations.Find(model.SelectedLocationId);
+                context.Locations.Remove(location);
+                context.SaveChanges();
+            }
 
             return RedirectToAction("Location");
             //todo create confirmation view!
--- TJS.VIMS/TJS.VIMS/DAL/OrganizationRepository.cs
+++ TJS.VIMS/TJS.VIMS/DAL/OrganizationRepository.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,8 +11,5 @@ namespace TJS.VIMS.DAL
         public OrganizationRepository(VIMSDBContext context) : base(context)
         {
         }
-
-        // humm?
-        
     }
 }
