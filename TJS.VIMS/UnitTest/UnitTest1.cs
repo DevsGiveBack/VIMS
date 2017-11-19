a003dffb4fc340c8c569754b243fd122e9023df0
Brian Preston <brian8181@gmail.com>
2017-11-19 16:37:59 -0600

    added UnitTest.csproj

---
+++ TJS.VIMS/UnitTest/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TJS.VIMS.DAL;
+using TJS.VIMS.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            using (VIMSDBContext context = new VIMSDBContext())
+            {
+                List<Country> list = context.Countries.ToList();
+                Assert.IsNotNull(context);
+                Assert.IsNotNull(list);
+            }
+        }
+    }
+}
