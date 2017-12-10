a0ae5739346e9993acfda731b648379b3a6f6737
Brian Preston <brian8181@gmail.com>
2017-12-10 08:15:05 -0600

    check in new files

---
+++ TJS.VIMS/TJS.VIMS/Models/MyTable2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TJS.VIMS.Models
+{
+    public class MyTable2
+    {
+        public long Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string OrganizationName { get; set; }
+    }
+}
